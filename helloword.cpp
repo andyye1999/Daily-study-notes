e9b4ef71a8d4d4ac135d1ac63ae7d8b459865a7d
redust <as_yhc@163.com>
2025-07-30 08:50:10 +0800

    vault backup: 2025-07-30 08:50:10

--- helloword.cpp
+++ helloword.cpp
@@ -1,8 +1,28 @@
 #include <bits/stdc++.h>
 
+using namespace std;
 
 // Simple Hello World program
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::vector<int> nums;
+    int num;
+    while (std::cin >> num)
+    {
+        nums.push_back(num);
+        // 读到换行符，终止循环
+        if (getchar() == '\n')
+        {
+            break;
+        }
+    }
+
+    // 验证是否读入成功
+    for (int i = 0; i < nums.size(); i++)
+    {
+        std::cout << nums[i] << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
